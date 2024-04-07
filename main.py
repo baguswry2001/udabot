90af169265604dc2d451e6a0fb35a43c3182a3bd
baguswirayuda <wirayuda101101@gmail.com>
2024-04-07 18:52:16 +0800

    tess

--- main.py
+++ main.py
@@ -1,4 +1,31 @@
-print("testy")
+from colorama import init, Fore
+
+# Inisialisasi colorama
+init()
+
+print(Fore.YELLOW + "testy")
 def apache():
-    print("cek")
-print("this muy ")
+    print(Fore.YELLOW + "cek")
+
+print(Fore.YELLOW + "          _____                   _____                   _____          ")
+print(Fore.YELLOW + "         /\    \                 /\    \                 /\    \         ")
+print(Fore.YELLOW + "        /::\____\               /::\    \               /::\    \        ")
+print(Fore.YELLOW + "       /:::/    /              /::::\    \             /::::\    \       ")
+print(Fore.YELLOW + "      /:::/    /              /::::::\    \           /::::::\    \      ")
+print(Fore.YELLOW + "     /:::/    /              /:::/\:::\    \         /:::/\:::\    \     ")
+print(Fore.YELLOW + "    /:::/    /              /:::/  \:::\    \       /:::/__\:::\    \    ")
+print(Fore.YELLOW + "   /:::/    /              /:::/    \:::\    \     /::::\   \:::\    \   ")
+print(Fore.YELLOW + "  /:::/    /      _____   /:::/    / \:::\    \   /::::::\   \:::\    \  ")
+print(Fore.YELLOW + " /:::/____/      /\    \ /:::/    /   \:::\ ___\ /:::/\:::\   \:::\    \ ")
+print(Fore.YELLOW + '|:::|    /      /::\____/:::/____/     \:::|    /:::/  \:::\   \:::\____\ ')
+print(Fore.YELLOW + "|:::|____\     /:::/    \:::\    \     /:::|____\::/    \:::\  /:::/    / ")
+print(Fore.YELLOW + " \:::\    \   /:::/    / \:::\    \   /:::/    / \/____/ \:::\/:::/    / ")
+print(Fore.YELLOW + "  \:::\    \ /:::/    /   \:::\    \ /:::/    /           \::::::/    /  ")
+print(Fore.YELLOW + "   \:::\    /:::/    /     \:::\    /:::/    /             \::::/    /   ")
+print(Fore.YELLOW + "    \:::\__/:::/    /       \:::\  /:::/    /              /:::/    /    ")
+print(Fore.YELLOW + "     \::::::::/    /         \:::\/:::/    /              /:::/    /     ")
+print(Fore.YELLOW + "      \::::::/    /           \::::::/    /              /:::/    /      ")
+print(Fore.YELLOW + "       \::::/    /             \::::/    /              /:::/    /       ")
+print(Fore.YELLOW + "        \::/____/               \::/____/               \::/    /        ")
+print(Fore.YELLOW + "         ~~                      ~~                      \/____/         ")
+                                                                         
