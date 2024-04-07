83a0abe27d2b1c1b13ba808ed6304f98b8f98e77
baguswirayuda <wirayuda101101@gmail.com>
2024-04-08 05:41:36 +0800

    update day 1,  5/4/2024

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import subprocess
 import platform
+from pytube import YouTube
 
 
 from pyfiglet import Figlet
@@ -31,13 +32,55 @@ print(Fore.GREEN+"+--------------------------------------+")
 print(horizontal_line)
 print(horizontal_line)
 
+
+def download_youtube_video(url, output_path=None):
+    try:
+        yt = YouTube(url)
+        video_stream = yt.streams.get_highest_resolution()
+        video_title = yt.title
+        file_size = video_stream.filesize
+        print(Fore.GREEN + "Judul video:", video_title)
+        print(Fore.GREEN + "Ukuran Video: {:.2f} MB".format(file_size / (1024 * 1024)))
+        print(Fore.CYAN + "Sedang mendownload. Sabar ya...")
+        if output_path:
+            video_stream.download(output_path)
+        else:
+            video_stream.download()
+        print(Fore.CYAN + "Download selesai.")
+    except Exception as e:
+        print(Fore.RED + "Gagal di-download. Periksa link atau jaringan cuy:", str(e))
+        
+    while True:
+        rep = input("Ingin mendownload video lagi [YA/NO] => ").lower()
+        if rep == "ya":
+            url=input("link video => ")
+            download_youtube_video(url)
+            break
+        elif rep == "no":
+            main()
+            break
+        else:
+            print(Fore.RED +"<!> MASUKAN PILIHAN YANG BENAR [ YA/NO ] <!>")
+   
+def download_audio_as_mp3(video_url):
+    try:
+        yt = YouTube(video_url)
+        audio_stream = yt.streams.filter(only_audio=True).first()
+        audio_stream.download(filename_prefix="audio_")
+        print("Unduhan audio berhasil")
+    except Exception as e:
+        print("Gagal mengunduh audio:", str(e))
+        
+        
+        
 def print_menu():
     print("+------------------------------------+")
     print("| No. |          Pilihan             |")
     print("+------------------------------------+")
     print("|  1  | internet speed test          |")
     print("|  2  | google translate             |")
-    print("|  0  | youtube viedo download       |")
+    print("|  3  | youtube video download       |")
+    print("|  0  | youtube audio download       |")
     print("+------------------------------------+")
 
 
@@ -45,10 +88,12 @@ def print_menu():
 def get_user_choice():
     while True:
         user_input = input("Masukkan pilihan Anda (1/2/0) : ")
-        if user_input in ['1', '2', '0']:
+        if user_input in ['1', '2', '3']:
             return user_input
         else:
             print("Pilihan tidak valid. Silakan masukkan nomor pilihan yang benar.")
+
+
 
 def main():
     while True:
@@ -63,11 +108,14 @@ def main():
                 subprocess.run(["python", "scraping1.py", ], check=True)
             except FileNotFoundError:
                 print("MAAF Ada kesalahan pastikan koneksi anda stabil")
-
+        elif user_choice == "3":
+            url=input("link video =>")
+            download_youtube_video(url)
 
         elif user_choice == '0':
             print("Terima kasih telah menggunakan program ini.")
             break
+
 
 if __name__ == "__main__":
     main()
